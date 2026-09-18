f8635735c6f13f530d10bc369edea9330c48084b
gutierrezjeralds <gutierrez.jeralds@yahoo.com>
2026-09-18 21:01:15 +0800

    Commit changes for adding css and google fonts

---
+++ client/src/locale/i18n.js
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import en from './en.json';
+
+i18n
+  // Automatically detects browser language (navigator.language)
+  .use(LanguageDetector)
+  // Connects with react-i18next
+  .use(initReactI18next)
+  .init({
+    // Do NOT set `lng` here so auto-detection works
+    fallbackLng: 'en', // Default to English if detected language isn't supported
+
+    resources: {
+      en: { translation: en }
+    },
+
+    interpolation: {
+      escapeValue: false // React handles XSS escaping
+    }
+  });
+
+export default i18n;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+import { CssBaseline } from '@mui/material';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './locale/i18n'; // Initializes i18n auto-detection on load
+import './components/assets/sass/screen.scss'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <CssBaseline />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
